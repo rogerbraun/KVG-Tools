57cb69382a5e8c565a4a57d8d01457a49002d23c
Roger Braun <roger.braun@student.uni-tuebingen.de>
2009-07-06 09:02:47 +0200

    Changed sexp_all_kanji.rb to work with the kanjisstrokes.txt from tagaini jisho repository.

--- sexp_all_kanji.rb
+++ sexp_all_kanji.rb
@@ -2,6 +2,8 @@ require "kvg2sexp.rb"
 
 
 IO.read("kanjisstrokes.txt").each do |line|
-  c = SVG_Character.new(line)
-  print c.to_sexp + "\n"
+  if ! line["#"]
+    c = SVG_Character.new(line)
+    print c.to_sexp + "\n"
+  end  
 end
